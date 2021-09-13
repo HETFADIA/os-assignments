99e3c1b02140f58860a1d64adc0fe53d67f5db91
Het <62541263+HETFADIA@users.noreply.github.com>
2021-09-13 14:56:00 +0530

    Create serialize.c

---
+++ Serialize-Deserialize/serialize.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void serialize(char * library_name,char * function_name,int library_name_size,int function_name_size){
+
+    char  serialize[5001];
+    int serializelen=0;
+    memset(serialize,'\0',sizeof(serialize));
+    for(int i=0;i<library_name_size;i++){
+        serialize[serializelen++]=library_name[i];
+    }
+    serialize[serializelen++]='#';
+    for(int i=0;i<function_name_size;i++){
+        serialize[serializelen++]=function_name[i];
+    }
+    printf("%s",serialize);
+}
+int main()
+{
+    
+    char * a="include";
+    char * b="function";
+    serialize(a,b,strlen(a),strlen(b));
+}
