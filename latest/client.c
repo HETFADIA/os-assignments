7aa3f5d6ccc7da0ebba90d055c80e0f323bc6c00
Het <62541263+HETFADIA@users.noreply.github.com>
2021-09-17 01:14:49 +0530

    Update client.c

--- latest/client.c
+++ latest/client.c
@@ -12,19 +12,15 @@
 typedef struct sockaddr_in IN;
 typedef struct sockaddr SA;
 
-void msg(char *msg, bool cond)
-{
+void msg(char *msg, bool ex){
     printf("%s\n", msg);
-    if (cond)
-        exit(-1);
+    if(ex) exit(-1);
 }
 
-void make_server(int PORT)
-{
+void client(int PORT){
 
     int _socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (_socket < 0)
-    {
+    if(_socket < 0){
         msg("SOCKET CREATION UNSUCCESSFUL", true);
     }
     int IN_size = sizeof(IN);
@@ -34,42 +30,33 @@ void make_server(int PORT)
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
     int _connect_status = connect(_socket, (SA *)&server_addr, IN_size);
-    if (_connect_status < 0)
-    {
+    if(_connect_status < 0) {
         msg("Connect failed", true);
     }
-    printf("Gaylmao\n");
-    for (int i = 0; i < 1; i++)
-    {
-        char buff[100];
-        memset(buff, '\0', sizeof(buff));
-        strncpy(buff, "/lib/libm.so.6#cos#2", sizeof(buff));
-        write(_socket, buff, sizeof(buff));
-        printf("Sent msg\n");
-        // sleep(1);
-        while (buff[0] != '#')
-        {
+    char **str = (char *[]){"/lib/x86_64-linux-gnu/libm.so.6#cos#2", "/lib/x86_64-linux-gnu/libm.so.6#sin#1", "/lib/x86_64-linux-gnu/libm.so.6#ceil#2.5", "/lib/x86_64-linux-gnu/libm.so.6#sqrt#10", "/lib/x86_64-linux-gnu/libm.so.6#log10#5", "/lib/x86_64-linux-gnu/libm.so.6#pow#2#5", "/lib/x86_64-linux-gnu/libm.so.6#hypot#3#4"};
+    for(int i=0;i<7;i++){
+      	char *buff = (char *)malloc(sizeof(char) * 201);
+        write(_socket, str[i], strlen(str[i]));
+        printf("Sent msg %s\n", str[i]);
+        while(buff[0]!='#'){
             int _recv_status = recv(_socket, buff, sizeof(buff), 0);
-            if (_recv_status <= 0)
-            {
+            if(_recv_status<=0){
                 printf("Not received");
                 exit(-1);
             }
-            else
-            {
+            else{
                 printf("received\n");
             }
         }
     }
     close(_socket);
+    
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
-        printf(".\a.out [PORT] [THREAD_LIMIT_DISPATCHER not for client] [FILE_LIMIT not for client] [MEMORY_LIMIT not for client]\n");
+int main(int argc, char **argv){
+    if(argc != 2){
+        printf(".\a.out [PORT]n");
         exit(-1);
     }
-    make_server(atoi(argv[1]));
+    client(atoi(argv[1]));
 }
