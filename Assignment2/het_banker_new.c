99b338f294a24efa15ad75ac311ad62017043ad5
Het <62541263+HETFADIA@users.noreply.github.com>
2021-10-08 18:35:00 +0530

    update

--- Assignment2/het_banker_new.c
+++ Assignment2/het_banker_new.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <arpa/inet.h>
+// #include <sys/socket.h>
+// #include <sys/un.h>
+// #include <arpa/inet.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <dlfcn.h>
-#include <sys/resource.h>
+// #include <sys/resource.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+pthread_mutex_t mutex_arr_of_resource = PTHREAD_MUTEX_INITIALIZER;
 int TOTAL_RESOURCES, TOTAL_THREADS, TIME_DELAY;
 int *arr_of_resources;
-int * time_stamp;
-int time_stamp_counter=0;
-bool * keep_alive;
+int *time_stamp;
+int time_stamp_counter = 0;
+bool *keep_alive;
 int **requests;
 int **max_requests;
 int min(int x, int y)
@@ -34,9 +34,9 @@ void *P_x(void *args)
 {
     int thread_num = *((int *)args);
     int arr[TOTAL_RESOURCES], save_temp[TOTAL_RESOURCES];
-    time_stamp[thread_num]=++time_stamp_counter;
-    while (keep_alive[thread_num])
+    while (1)
     {
+        time_stamp[thread_num] = ++time_stamp_counter;
         int TRACK = TOTAL_RESOURCES;
         for (int i = 0; i < TOTAL_RESOURCES; i++)
         {
@@ -45,10 +45,10 @@ void *P_x(void *args)
                 TRACK -= 1;
             save_temp[i] = arr[i];
             requests[thread_num][i] = arr[i];
-            max_requests[thread_num][i]=save_temp[i];
+            max_requests[thread_num][i] = save_temp[i];
         }
         // TRACK the requirement of resources, if none end looping
-        while (TRACK > 0)
+        while (TRACK > 0 && keep_alive[thread_num])
         {
             int resource_for_now = rand() % TOTAL_RESOURCES;
 
@@ -56,8 +56,10 @@ void *P_x(void *args)
                 continue;
 
             //mutex lock part 1
+            pthread_mutex_lock(&mutex_arr_of_resource);
             int taken = min(arr_of_resources[resource_for_now], arr[resource_for_now]);
             arr_of_resources[resource_for_now] -= taken;
+            pthread_mutex_unlock(&mutex_arr_of_resource);
             //mutex unlock
 
             arr[resource_for_now] -= taken;
@@ -69,6 +71,7 @@ void *P_x(void *args)
             }
             sleep(rand() % TIME_DELAY + 1);
         }
+        keep_alive[thread_num]=1;
         // sleep for (0.7d, 1.5d);
         usleep((7 * TIME_DELAY + rand() % TIME_DELAY * 8) * 1e5);
         // run thread again like normal people
@@ -77,68 +80,86 @@ void *P_x(void *args)
         for (int i = 0; i < TOTAL_RESOURCES; i++)
         {
             //mutex lock 2
-            arr_of_resources[i] += save_temp[i];
+            pthread_mutex_lock(&mutex_arr_of_resource);
+            arr_of_resources[i] += (save_temp[i]-arr[i]);
+            pthread_mutex_unlock(&mutex_arr_of_resource);
             //mutex unlock
         }
     }
     return NULL;
 }
-int sum(int * arr,int len){
-    int res=0;
-    for(int i=0;i<len;i++){
-        res+=arr[i];
+int sum(int *arr, int len)
+{
+    int res = 0;
+    for (int i = 0; i < len; i++)
+    {
+        res += arr[i];
     }
     return res;
 }
-int heuristics1(bool * arr_involved_in_deadlock){
+int heuristics1(bool *arr_involved_in_deadlock)
+{
     //selects the resource having max sum of resources needed(max(sum(needed resources)))
-    int to_be_removed=0;
-    int to_be_removed_sum=-1;
-    for(int i=0;i<TOTAL_THREADS;i++){
-        int temp_sum=sum(requests[i],TOTAL_RESOURCES);
-        if(arr_involved_in_deadlock[i] && temp_sum>to_be_removed_sum){
-            to_be_removed=i;
-            to_be_removed_sum=temp_sum;
+    int to_be_removed = 0;
+    int to_be_removed_sum = -1;
+    for (int i = 0; i < TOTAL_THREADS; i++)
+    {
+        int temp_sum = sum(requests[i], TOTAL_RESOURCES);
+        if (arr_involved_in_deadlock[i] && temp_sum > to_be_removed_sum)
+        {
+            to_be_removed = i;
+            to_be_removed_sum = temp_sum;
         }
     }
     return to_be_removed;
 }
-int heuristics2(bool * arr_involved_in_deadlock){
+int heuristics2(bool *arr_involved_in_deadlock)
+{
     //selects youngest thread i.e. having max time_stamp
-    int to_be_removed=0;
-    int to_be_removed_time=-1;
-    for(int i=0;i<TOTAL_THREADS;i++){
-        
-        if(arr_involved_in_deadlock[i] && time_stamp[i]>to_be_removed_time){
-            to_be_removed=i;
-            to_be_removed_time=to_be_removed_time;
+    int to_be_removed = 0;
+    int to_be_removed_time = -1;
+    for (int i = 0; i < TOTAL_THREADS; i++)
+    {
+
+        if (arr_involved_in_deadlock[i] && time_stamp[i] > to_be_removed_time)
+        {
+            to_be_removed = i;
+            to_be_removed_time = to_be_removed_time;
         }
     }
     return to_be_removed;
 }
-int heuristics3(bool * arr_involved_in_deadlock){
+int heuristics3(bool *arr_involved_in_deadlock)
+{
     //selects the resource having max sum of resources max_needed(max(sum(max_needed)))
-    int to_be_removed=0;
-    int to_be_removed_sum=-1;
-    for(int i=0;i<TOTAL_THREADS;i++){
-        int temp_sum=sum(max_requests[i],TOTAL_RESOURCES);
-        if(arr_involved_in_deadlock[i] && temp_sum>to_be_removed_sum){
-            to_be_removed=i;
-            to_be_removed_sum=temp_sum;
+    int to_be_removed = 0;
+    int to_be_removed_sum = -1;
+    for (int i = 0; i < TOTAL_THREADS; i++)
+    {
+        int temp_sum = sum(max_requests[i], TOTAL_RESOURCES);
+        if (arr_involved_in_deadlock[i] && temp_sum > to_be_removed_sum)
+        {
+            to_be_removed = i;
+            to_be_removed_sum = temp_sum;
         }
     }
     return to_be_removed;
 }
-int heuristics4(bool * arr_involved_in_deadlock){
-    //selects max resource 
-    int to_be_removed=0;
-    int to_be_removed_value=-1;
-    for(int i=0;i<TOTAL_THREADS;i++){
-        if(arr_involved_in_deadlock[i]){
-            for(int j=0;j<TOTAL_RESOURCES;j++){
-                if(requests[i][j]>to_be_removed_value){
-                    to_be_removed=i;
-                    to_be_removed_value=request[i][j];
+int heuristics4(bool *arr_involved_in_deadlock)
+{
+    //selects max resource
+    int to_be_removed = 0;
+    int to_be_removed_value = -1;
+    for (int i = 0; i < TOTAL_THREADS; i++)
+    {
+        if (arr_involved_in_deadlock[i])
+        {
+            for (int j = 0; j < TOTAL_RESOURCES; j++)
+            {
+                if (requests[i][j] > to_be_removed_value)
+                {
+                    to_be_removed = i;
+                    to_be_removed_value = requests[i][j];
                 }
             }
         }
@@ -148,32 +169,52 @@ int heuristics4(bool * arr_involved_in_deadlock){
 
 void *deadlock_detection(void *args)
 {
+    int thread_num = *((int *)args);
     //run this thread infinitely
 
     //do something, I forgor 💀
-    bool deadlock_found=1;
+    bool deadlock_found = 1;
     bool arr_involved_in_deadlock[TOTAL_THREADS];
 
     for (int i = 0; i < TOTAL_THREADS; i++)
     {
-        bool ith_thread_can_go=1;
-        arr_involved_in_deadlock[i]=0;
+        bool ith_thread_can_go = 1;
+        arr_involved_in_deadlock[i] = 0;
         for (int j = 0; j < TOTAL_RESOURCES; j++)
         {
-            if(arr_of_resources[j]<requests[i][j]){
-                ith_thread_can_go=0;
+            if (arr_of_resources[j] < requests[i][j])
+            {
+                ith_thread_can_go = 0;
                 break;
             }
         }
-        if(ith_thread_can_go){
-            deadlock_found=0;
+        if (ith_thread_can_go)
+        {
+            deadlock_found = 0;
         }
-        else{
-            arr_involved_in_deadlock[i]=1;
+        else
+        {
+            arr_involved_in_deadlock[i] = 1;
         }
     }
-    int to_be_removed=heuristics1(arr_involved_in_deadlock);
-
+    int to_be_removed;
+    if (thread_num == 0)
+    {
+        to_be_removed = heuristics1(arr_involved_in_deadlock);
+    }
+    else if (thread_num == 1)
+    {
+        to_be_removed = heuristics2(arr_involved_in_deadlock);
+    }
+    else if (thread_num == 2)
+    {
+        to_be_removed = heuristics3(arr_involved_in_deadlock);
+    }
+    else if (thread_num == 3)
+    {
+        to_be_removed = heuristics4(arr_involved_in_deadlock);
+    }
+    keep_alive[to_be_removed]=0;
     sleep(TIME_DELAY);
     return NULL;
 }
@@ -191,19 +232,18 @@ int main(int argc, char **argv)
     for (int i = 0; i < TOTAL_RESOURCES; i++)
     {
         arr_of_resources[i] = stoi(argv[i + 2]);
-        
     }
     TOTAL_THREADS = stoi(argv[TOTAL_RESOURCES + 2]);
     requests = (int **)malloc(sizeof(int *) * TOTAL_THREADS);
     max_requests = (int **)malloc(sizeof(int *) * TOTAL_THREADS);
-    keep_alive=(bool *)malloc(sizeof(bool)*TOTAL_THREADS);
-    time_stamp=(int *)malloc(sizeof(int)*TOTAL_THREADS);
+    keep_alive = (bool *)malloc(sizeof(bool) * TOTAL_THREADS);
+    time_stamp = (int *)malloc(sizeof(int) * TOTAL_THREADS);
     for (int i = 0; i < TOTAL_THREADS; i++)
     {
         requests[i] = (int *)malloc(sizeof(int) * TOTAL_RESOURCES);
         max_requests[i] = (int *)malloc(sizeof(int) * TOTAL_RESOURCES);
-        keep_alive[i]=1;
-        time_stamp[i]=0;
+        keep_alive[i] = 1;
+        time_stamp[i] = 0;
     }
     TIME_DELAY = stoi(argv[TOTAL_RESOURCES + 3]);
 }
